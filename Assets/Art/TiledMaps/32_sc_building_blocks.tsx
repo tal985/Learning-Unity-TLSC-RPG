5880f73ea041ff082284ff853cf25c27dd470816
tal985 <tal985@utexas.edu>
2019-01-05 18:41:00 -0600

    Moving to PC
    1/5/19 6:39 PM

---
+++ Assets/Art/TiledMaps/32_sc_building_blocks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="32_building_blocks" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <image source="32_building_blocks.png" width="224" height="224"/>
+</tileset>
---
+++ Assets/Art/TiledMaps/collision_mask_1.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="collision_mask_1" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="collision_mask_1.png" width="32" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="11" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
